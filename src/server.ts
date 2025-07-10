172a7535ec1ba38062c4a3a7d71215b015ed11a9
Laize Ferraz <laizeferraz@gmail.com>
2025-07-10 19:58:26 +1000

    add questions routes and create room route

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,9 @@ import {
 // To test the database connection, you can uncomment the import below
 // import { sql } from './db/connection.ts';
 import { env } from './env.ts';
+import { createQuestionRoute } from './http/routes/create-question.ts';
+import { createRoomRoute } from './http/routes/create-room.ts';
+import { getRoomQuestionsRoute } from './http/routes/get-room-questions.ts';
 import { getRoomsRoute } from './http/routes/get-rooms.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
@@ -24,6 +27,9 @@ app.get('/health', () => {
 });
 
 app.register(getRoomsRoute);
+app.register(createRoomRoute);
+app.register(getRoomQuestionsRoute);
+app.register(createQuestionRoute);
 
 app.listen({
   port: env.PORT,
