3295f812bf732806d06d6e1260a2322738d7d6d5
Laize Ferraz <laizeferraz@gmail.com>
2025-08-06 18:06:18 +1000

    update node version & file extension - try 39

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   entry: ['src/**/*.ts', 'api/**/*.ts'],
   outDir: 'dist',
   format: ['esm'],
-  target: 'node18',
+  target: 'node22',
   clean: true,
   splitting: false,
   dts: false,
