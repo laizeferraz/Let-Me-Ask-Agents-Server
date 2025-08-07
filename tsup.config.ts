6fdf5f4d260bfa2637df778011372c1c72a0158f
Laize Ferraz <laizeferraz@gmail.com>
2025-08-07 17:42:08 +1000

    fix deployment - try47

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/**/*.ts', 'api/*.ts'],
-  outDir: 'dist',
+  outDir: 'api',
   format: ['esm'],
   target: 'node22',
   clean: true,
