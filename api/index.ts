a1659c776935060d3ee3229e90c4c4c45ce2d25d
Laize Ferraz <laizeferraz@gmail.com>
2025-09-04 15:58:47 +1000

    remove vercel setup

--- api/index.ts
+++
@@ -1,7 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-import app from '../src/server';
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  await app.ready();
-  app.server.emit('request', req, res);
-}
